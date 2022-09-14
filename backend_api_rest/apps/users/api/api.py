a54910d846e6e2149e510d3f9f928a2266d91bac
Chris <chrisdeveloper3001@gmail.com>
2022-09-14 13:14:05 -0300

    wip(init): add basic project rest structure

---
+++ backend_api_rest/apps/users/api/api.py
@@ -0,0 +1,55 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from apps.users.models import User
+from apps.users.api.serializer import UserSerializer
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def user_api_view(request):
+  
+  # list resources
+  if request.method == 'GET':
+    users = User.objects.all()
+    users_serializer = UserSerializer(users, many = True) # (many = True) ---> List of users, it expects only 1 object
+    return Response(users_serializer.data, status.HTTP_200_OK)
+
+  # create resource
+  if request.method == 'POST':
+    user_serializer = UserSerializer(data = request.data) # get json and compare to model
+    if(user_serializer.is_valid()):
+      user_serializer.save()
+      return Response({'message': 'User has been created successfully!'}, status = status.HTTP_200_OK)
+    
+    return Response(user_serializer.errors, status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail_api_view(request, pk=None):
+
+  user = User.objects.filter(id = pk).first()
+
+  if user is None:
+    return Response({'message': 'User not found'}, status = status.HTTP_400_BAD_REQUEST)
+  
+  # get resource
+  if request.method == 'GET':
+    user_serializer = UserSerializer(user)
+    return Response(user_serializer.data, status.HTTP_200_OK)
+
+  # update resource
+  if request.method == 'PUT':
+    user_serializer = UserSerializer(user, data = request.data)
+    if user_serializer.is_valid():
+      user_serializer.save()
+      return Response(user_serializer.data, status.HTTP_200_OK)
+    
+    return Response(user_serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+  # delete resource
+  if request.method == 'DELETE':
+    user.delete() 
+    return Response({'message': 'User has been deleted successfully!'}, status = status.HTTP_200_OK)
+  
+  
+    
---
+++ backend_api_rest/backend_api_rest/settings/local.py
@@ -0,0 +1,25 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_pretty_db',
+        'HOST': 'localhost',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': '123456'
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = 'static/'
